624da8f80abccf3677cef746373c9a86b77e53ae
dasjayadev <jayadev.das2581@gmail.com>
2024-06-16 20:20:13 +0530

    keyHandler complited

--- src/components/OtpInput.jsx
+++ src/components/OtpInput.jsx
@@ -1,57 +1,74 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 
-const OtpInput = ({length=6, onOtpSubmit=()=>{}}) => {
+const OtpInput = ({ length = 6, onOtpSubmit = () => {} }) => {
+  let [otp, setOtp] = useState(new Array(length).fill(""));
+  let inputRef = useRef([]);
 
-    let [otp,setOtp] = useState(new Array(length).fill(""));
-    let inputRef = useRef([]);
+  useEffect(() => {
+    if (inputRef.current[0]) {
+      inputRef.current[0].focus();
+    }
+  }, []);
 
-    useEffect(()=>{
-        if(inputRef.current[0]){
-            inputRef.current[0].focus();
-        }
-    },[])
+  // console.log(inputRef);
 
-    // console.log(inputRef);
+  let changeHandeler = (index, event) => {
+    let value = event.target.value;
+    if (isNaN(value)) return;
 
-    let changeHandeler = (index,event)=>{
-        let value = event.target.value;
-        if(isNaN(value)) return
+    let newOtp = [...otp];
 
-        let newOtp = [...otp];
-        //allow only one input
-        newOtp[index] = value.substring(value.length -1);
-        setOtp(newOtp)
+    //allow only one input
+    newOtp[index] = value.substring(value.length - 1);
+    setOtp(newOtp);
 
-        //submit trigger
-        let combinOtp = newOtp.join('');
-        // console.log(newOtp ,combinOtp)
-        if(combinOtp.length === length) onOtpSubmit(combinOtp)
-        //Move to next input field if current field is filled
-        if(value && index < length - 1 && inputRef.current[index+1]){
-            inputRef.current[index + 1].focus();
-        }
-    };
-    let clickHandler = ()=>{}
-    let keyDownHandler = ()=>{}
+    //submit trigger
+    let combinOtp = newOtp.join("");
+
+    // console.log(newOtp ,combinOtp)
+    if (combinOtp.length === length) onOtpSubmit(combinOtp);
+
+    //Move to next input field if current field is filled
+    if (value && index < length - 1 && inputRef.current[index + 1]) {
+      inputRef.current[index + 1].focus();
+    }
+  };
+
+  
+  let clickHandler = () => {};
+
+
+  let keyDownHandler = (index, event) => {
+    if (
+      event.key === "Backspace" &&
+      !otp[index] &&
+      index > 0 &&
+      inputRef.current[index - 1]
+    ) {
+
+    //move focus to previous input field on backspace
+      inputRef.current[index - 1].focus();
+    }
+  };
 
   return (
     <>
-     {
-        otp.map((value,index)=>{
-            return <input key={index} 
+      {otp.map((value, index) => {
+        return (
+          <input
+            key={index}
             type="text"
             value={value}
-            ref={(input)=>inputRef.current[index] = input}
-            onChange={(event)=>changeHandeler(index,event)}
-            onClick={()=>clickHandler(index)}
-            onKeyDown={(event)=>keyDownHandler(index, event)}
-            className='otpinput'
-             />
-        })
-     }
+            ref={(input) => (inputRef.current[index] = input)}
+            onChange={(event) => changeHandeler(index, event)}
+            onClick={() => clickHandler(index)}
+            onKeyDown={(event) => keyDownHandler(index, event)}
+            className="otpinput"
+          />
+        );
+      })}
     </>
   );
-}
+};
 
 export default OtpInput;
-
